e4fb5ec49e1a2fdbd5f23c8f31f3cac67e290582
kepha222 <kephamotari1@.com>
2022-10-18 00:28:32 +0300

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./features/modal/modalSlice";
+import "./features/cart/cartSlice";
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
+});
